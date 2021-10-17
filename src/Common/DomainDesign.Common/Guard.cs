5aa618cf1689917db1f6301edd645570be530ca3
Darren Powell <darren.powell@hotmail.co.uk>
2021-10-18 00:23:41 +0100

    Add initial version of Tournament Entity

--- src/Common/DomainDesign.Common/Guard.cs
+++ src/Common/DomainDesign.Common/Guard.cs
@@ -4,6 +4,14 @@ namespace DomainDesign.Common
 {
 	public static class Guard
 	{
+		public static void ForNullOrEmptyString(string value, string parameterName)
+		{
+			if (string.IsNullOrEmpty(value))
+			{
+				throw new ArgumentException("Value can not be null or empty string", parameterName);
+			}
+		}
+
 		public static void ForIntegerOutOfRange(int value, int lowLimit, int highLimit, string parameterName)
 		{
 			if (value < lowLimit || value > highLimit)
---
+++ src/TournamentManagement/TournamentManagement.Domain/Tournament.cs
@@ -0,0 +1,32 @@
+﻿using DomainDesign.Common;
+using System;
+
+namespace TournamentManagement.Domain
+{
+	public class Tournament : Entity<Guid>
+	{
+		public string Title { get; private set; }
+		public TournamentDates Dates { get; private set; }
+
+		public int Year => Dates.Year;
+		public DateTime StartDate => Dates.StartDate;
+		public DateTime EndDate => Dates.EndDate;
+
+		private Tournament(Guid id) : base(id)
+		{
+		}
+
+		public static Tournament Create(string title, DateTime startDate, DateTime endDate)
+		{
+			Guard.ForNullOrEmptyString(title, "title");
+
+			var tournament = new Tournament(Guid.NewGuid())
+			{
+				Title = title,
+				Dates = new TournamentDates(startDate, endDate)
+			};
+
+			return tournament;
+		}
+	}
+}
