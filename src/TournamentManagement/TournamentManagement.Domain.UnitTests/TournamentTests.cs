cbd91949a57148137ab138be0e562c89cecb2baf
Darren Powell <darren.powell@hotmail.co.uk>
2021-10-20 22:24:09 +0100

    Extend tournament entity (#6)
    * Add State property to the Tournament entity
    
    * Add Level property to the Tournament entity
    
    * Allow details of a Tournament to be updated

--- src/TournamentManagement/TournamentManagement.Domain.UnitTests/TournamentTests.cs
+++ src/TournamentManagement/TournamentManagement.Domain.UnitTests/TournamentTests.cs
@@ -7,13 +7,16 @@ namespace TournamentManagement.Domain.UnitTests
 	public class TournamentTests
 	{
 		[Fact]
-		public void CanUseFactoryMethodToCreateTournament()
+		public void CanUseFactoryMethodToCreateTournamentAndItIsCreatedCorrectly()
 		{
-			var tournament = Tournament.Create("Wimbledon", new DateTime(2019, 7, 1), new DateTime(2019, 7, 14));
+			var tournament = Tournament.Create("Wimbledon", TournamentLevel.GrandSlam,
+				new DateTime(2019, 7, 1), new DateTime(2019, 7, 14));
 
 			tournament.Id.Should().NotBe(Guid.Empty);
 			tournament.Title.Should().Be("Wimbledon");
 			tournament.Year.Should().Be(2019);
+			tournament.Level.Should().Be(TournamentLevel.GrandSlam);
+			tournament.State.Should().Be(TournamentState.BeingDefined);
 			tournament.StartDate.Should().Be(new DateTime(2019, 7, 1));
 			tournament.EndDate.Should().Be(new DateTime(2019, 7, 14));
 		}
@@ -23,11 +26,28 @@ namespace TournamentManagement.Domain.UnitTests
 		[InlineData("")]
 		public void CannotCreateTournamentWithEmptyTitle(string title)
 		{
-			Action act = () => Tournament.Create(title, new DateTime(2019, 7, 1), new DateTime(2019, 7, 14));
+			Action act = () => Tournament.Create(title, TournamentLevel.Masters125,
+				new DateTime(2019, 7, 1), new DateTime(2019, 7, 14));
 
 			act.Should()
 				.Throw<ArgumentException>()
 				.WithMessage("Value can not be null or empty string (Parameter 'title')");
+		}
+
+		[Fact]
+		public void CanUpdateTournamentDetails()
+		{
+			var tournament = Tournament.Create("Wimbledon", TournamentLevel.GrandSlam,
+				new DateTime(2019, 7, 1), new DateTime(2019, 7, 14));
+
+			tournament.UpdateDetails("New Wimbledon", TournamentLevel.Masters500,
+				new DateTime(2019, 7, 4), new DateTime(2019, 7, 17));
+
+			tournament.Title.Should().Be("New Wimbledon");
+			tournament.Level.Should().Be(TournamentLevel.Masters500);
+			tournament.State.Should().Be(TournamentState.BeingDefined);
+			tournament.StartDate.Should().Be(new DateTime(2019, 7, 4));
+			tournament.EndDate.Should().Be(new DateTime(2019, 7, 17));
 		}
 	}
 }
--- src/TournamentManagement/TournamentManagement.Domain/Tournament.cs
+++ src/TournamentManagement/TournamentManagement.Domain/Tournament.cs
@@ -7,6 +7,8 @@ namespace TournamentManagement.Domain
 	{
 		public string Title { get; private set; }
 		public TournamentDates Dates { get; private set; }
+		public TournamentState State { get; private set; }
+		public TournamentLevel Level { get; private set; }
 
 		public int Year => Dates.Year;
 		public DateTime StartDate => Dates.StartDate;
@@ -16,17 +18,33 @@ namespace TournamentManagement.Domain
 		{
 		}
 
-		public static Tournament Create(string title, DateTime startDate, DateTime endDate)
+		public static Tournament Create(string title, TournamentLevel level, DateTime startDate, DateTime endDate)
 		{
 			Guard.ForNullOrEmptyString(title, "title");
 
 			var tournament = new Tournament(Guid.NewGuid())
 			{
 				Title = title,
+				Level = level,
+				State = TournamentState.BeingDefined,
 				Dates = new TournamentDates(startDate, endDate)
 			};
 
 			return tournament;
+		}
+
+		public void UpdateDetails(string title, TournamentLevel level, DateTime startDate, DateTime endDate)
+		{
+			if (State != TournamentState.BeingDefined)
+			{
+				throw new Exception("Can only change details of the Tournament before it is opened for entry.");
+			}
+
+			Guard.ForNullOrEmptyString(title, "title");
+
+			Title = title;
+			Level = level;
+			Dates = new TournamentDates(startDate, endDate);
 		}
 	}
 }
