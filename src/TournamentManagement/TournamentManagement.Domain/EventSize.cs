513faf3cc71ae194b7572809e1e700c61fe8687d
Darren Powell <darren.powell@hotmail.co.uk>
2021-10-21 23:03:53 +0100

    Add EventSize Value Object

---
+++ src/TournamentManagement/TournamentManagement.Domain/EventSize.cs
@@ -0,0 +1,44 @@
+﻿using DomainDesign.Common;
+using System;
+
+namespace TournamentManagement.Domain
+{
+	public class EventSize : ValueObject<EventSize>
+	{
+		private const int MaxNumberOfEntrants = 128;
+		private static readonly int[] AllowedNumberOfSeeds = { 2, 4, 8, 16, 32 };
+		private static readonly int[] EntrantsPerRound = { 2, 4, 8, 16, 32, 64, 128 };
+		private static readonly int[] MinAllowedEntrants = { 2, 2, 4, 8, 16, 32, 64 };
+
+		public int EntrantsLimit { get; }
+		public int NumberOfSeeds { get; }
+		public int NumberOfRounds { get; }
+		public int MinimumEntrants { get; }
+
+		public EventSize(int entrantsLimit, int numberOfSeeds)
+		{
+			Guard.ForValueNotInSetOfAllowedValues(numberOfSeeds, AllowedNumberOfSeeds, "numberOfSeeds");
+			Guard.ForIntegerOutOfRange(entrantsLimit, numberOfSeeds, MaxNumberOfEntrants, "entrantsLimit");
+
+			EntrantsLimit = entrantsLimit;
+			NumberOfSeeds = numberOfSeeds;
+			NumberOfRounds = CalculateNumberOfRounds(entrantsLimit);
+			MinimumEntrants = MinAllowedEntrants[NumberOfRounds - 1]; 
+		}
+
+		private static int CalculateNumberOfRounds(int entrantsLimit)
+		{
+			int rounds = 0;
+			for (int i = 0; i < EntrantsPerRound.Length; i++)
+			{
+				if (EntrantsPerRound[i] >= entrantsLimit)
+				{
+					rounds = i+1;
+					break;
+				}
+			}
+
+			return rounds;
+		}
+	}
+}
