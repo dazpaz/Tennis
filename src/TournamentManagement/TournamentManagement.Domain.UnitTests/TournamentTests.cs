9c26f91419e10d5b6f9360c9a020345da4239f77
Darren Powell <darren.powell@hotmail.co.uk>
2021-10-22 23:43:28 +0100

    Add a collection of Events to the Tournament (#8)

--- src/TournamentManagement/TournamentManagement.Domain.UnitTests/TournamentTests.cs
+++ src/TournamentManagement/TournamentManagement.Domain.UnitTests/TournamentTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace TournamentManagement.Domain.UnitTests
@@ -48,6 +49,159 @@ namespace TournamentManagement.Domain.UnitTests
 			tournament.State.Should().Be(TournamentState.BeingDefined);
 			tournament.StartDate.Should().Be(new DateTime(2019, 7, 4));
 			tournament.EndDate.Should().Be(new DateTime(2019, 7, 17));
+		}
+
+		[Fact]
+		public void CanAddAnEventToATournament()
+		{
+			var tournament = Tournament.Create("Wimbledon", TournamentLevel.GrandSlam,
+				new DateTime(2019, 7, 1), new DateTime(2019, 7, 14));
+			var tennisEvent = Event.Create(EventType.MensSingles, 128, 32,
+				MatchFormat.ThreeSetMatchWithFinalSetTieBreak);
+
+			tournament.AddEvent(tennisEvent);
+
+			tournament.Events.Count.Should().Be(1);
+			tournament.Events[EventType.MensSingles].EventSize.EntrantsLimit.Should().Be(128);
+		}
+
+		[Fact]
+		public void CanAddAllTypesOfEventToATournament()
+		{
+			var tournament = Tournament.Create("Wimbledon", TournamentLevel.GrandSlam,
+				new DateTime(2019, 7, 1), new DateTime(2019, 7, 14));
+
+			tournament.AddEvent(Event.Create(EventType.MensSingles, 128, 32,
+				MatchFormat.FiveSetMatchWithTwoGamesClear));
+			tournament.AddEvent(Event.Create(EventType.WomensSingles, 128, 32,
+				MatchFormat.ThreeSetMatchWithTwoGamesClear));
+			tournament.AddEvent(Event.Create(EventType.MensDoubles, 64, 16,
+				MatchFormat.FiveSetMatchWithFinalSetTieBreak));
+			tournament.AddEvent(Event.Create(EventType.WomensDoubles, 64, 16,
+				MatchFormat.ThreeSetMatchWithFinalSetTieBreak));
+			tournament.AddEvent(Event.Create(EventType.MixedDoubles, 32, 8,
+				new MatchFormat(3, FinalSetType.ChampionsTieBreak)));
+
+			tournament.Events.Count.Should().Be(5);
+		}
+
+		[Fact]
+		public void CanRemoveAnEventFromATournament()
+		{
+			var tournament = Tournament.Create("Wimbledon", TournamentLevel.GrandSlam,
+				new DateTime(2019, 7, 1), new DateTime(2019, 7, 14));
+
+			tournament.AddEvent(Event.Create(EventType.MensSingles, 128, 32,
+				MatchFormat.FiveSetMatchWithTwoGamesClear));
+			tournament.AddEvent(Event.Create(EventType.WomensSingles, 64, 16,
+				MatchFormat.ThreeSetMatchWithTwoGamesClear));
+
+			tournament.Events.Count.Should().Be(2);
+
+			tournament.RemoveEvent(EventType.MensSingles);
+
+			tournament.Events.Count.Should().Be(1);
+			tournament.Events[EventType.WomensSingles].EventSize.EntrantsLimit.Should().Be(64);
+		}
+
+		[Fact]
+		public void CannnotAddASecondEventIfTheSameTypeToATournament()
+		{
+			var tournament = Tournament.Create("Wimbledon", TournamentLevel.GrandSlam,
+				new DateTime(2019, 7, 1), new DateTime(2019, 7, 14));
+			tournament.AddEvent(Event.Create(EventType.MensSingles, 128, 32,
+				MatchFormat.FiveSetMatchWithTwoGamesClear));
+
+			Action act = () => tournament.AddEvent(Event.Create(EventType.MensSingles, 128, 32,
+				MatchFormat.FiveSetMatchWithTwoGamesClear));
+
+			act.Should()
+				.Throw<Exception>()
+				.WithMessage("Tournament already has an event of type MensSingles");
+		}
+
+		[Fact]
+		public void CannotRemoveAnEventTypeFromATournamentIfItDoesNotExist()
+		{
+			var tournament = Tournament.Create("Wimbledon", TournamentLevel.GrandSlam,
+				new DateTime(2019, 7, 1), new DateTime(2019, 7, 14));
+
+			tournament.AddEvent(Event.Create(EventType.MensSingles, 128, 32,
+				MatchFormat.FiveSetMatchWithTwoGamesClear));
+
+			tournament.Events.Count.Should().Be(1);
+
+			Action act = () => tournament.RemoveEvent(EventType.WomensSingles);
+
+			act.Should()
+				.Throw<Exception>()
+				.WithMessage("Tournament does not have an event of type WomensSingles");
+		}
+
+		[Fact]
+		public void CanClearTheEventsForTheTournament()
+		{
+			var tournament = Tournament.Create("Wimbledon", TournamentLevel.GrandSlam,
+				new DateTime(2019, 7, 1), new DateTime(2019, 7, 14));
+
+			tournament.AddEvent(Event.Create(EventType.MensSingles, 128, 32,
+				MatchFormat.FiveSetMatchWithTwoGamesClear));
+			tournament.AddEvent(Event.Create(EventType.WomensSingles, 128, 32,
+				MatchFormat.ThreeSetMatchWithTwoGamesClear));
+
+			tournament.Events.Count.Should().Be(2);
+
+			tournament.ClearEvents();
+
+			tournament.Events.Count.Should().Be(0);
+		}
+
+		[Fact]
+		public void CanAddACollectionOfEventsToATournamment()
+		{
+			var events = new List<Event>()
+			{
+				Event.Create(EventType.MensSingles, 128, 32,
+					MatchFormat.FiveSetMatchWithTwoGamesClear),
+				Event.Create(EventType.WomensSingles, 128, 32,
+					MatchFormat.FiveSetMatchWithTwoGamesClear),
+				Event.Create(EventType.MensDoubles, 64, 16,
+					MatchFormat.FiveSetMatchWithFinalSetTieBreak),
+				Event.Create(EventType.WomensDoubles, 64, 16,
+				MatchFormat.ThreeSetMatchWithFinalSetTieBreak),
+					Event.Create(EventType.MixedDoubles, 32, 8,
+				new MatchFormat(3, FinalSetType.ChampionsTieBreak))
+			};
+
+			var tournament = Tournament.Create("Wimbledon", TournamentLevel.GrandSlam,
+				new DateTime(2019, 7, 1), new DateTime(2019, 7, 14));
+
+			tournament.SetEvents(events);
+
+			tournament.Events.Count.Should().Be(5);
+		}
+
+		[Fact]
+		public void CannotAddACollectionOfEventsToATournammentIfTwoHaveTheSameType()
+		{
+			var events = new List<Event>()
+			{
+				Event.Create(EventType.MensSingles, 128, 32,
+					MatchFormat.FiveSetMatchWithTwoGamesClear),
+				Event.Create(EventType.MensSingles, 128, 32,
+					MatchFormat.FiveSetMatchWithTwoGamesClear)
+			};
+
+			var tournament = Tournament.Create("Wimbledon", TournamentLevel.GrandSlam,
+				new DateTime(2019, 7, 1), new DateTime(2019, 7, 14));
+
+			Action act = () => tournament.SetEvents(events);
+
+			act.Should()
+				.Throw<Exception>()
+				.WithMessage("Tournament already has an event of type MensSingles");
+
+			tournament.Events.Count.Should().Be(0);
 		}
 	}
 }
--- src/TournamentManagement/TournamentManagement.Domain/Tournament.cs
+++ src/TournamentManagement/TournamentManagement.Domain/Tournament.cs
@@ -1,5 +1,7 @@
 ﻿using DomainDesign.Common;
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace TournamentManagement.Domain
 {
@@ -14,8 +16,14 @@ namespace TournamentManagement.Domain
 		public DateTime StartDate => Dates.StartDate;
 		public DateTime EndDate => Dates.EndDate;
 
+		public IReadOnlyDictionary<EventType, Event> Events { get; private set; }
+
+		private readonly IDictionary<EventType, Event> _events;
+
 		private Tournament(Guid id) : base(id)
 		{
+			_events = new Dictionary<EventType, Event>();
+			Events = new ReadOnlyDictionary<EventType, Event>(_events);
 		}
 
 		public static Tournament Create(string title, TournamentLevel level, DateTime startDate, DateTime endDate)
@@ -35,16 +43,80 @@ namespace TournamentManagement.Domain
 
 		public void UpdateDetails(string title, TournamentLevel level, DateTime startDate, DateTime endDate)
 		{
-			if (State != TournamentState.BeingDefined)
-			{
-				throw new Exception("Can only change details of the Tournament before it is opened for entry.");
-			}
-
+			GuardForActionInCorrectState(TournamentState.BeingDefined, "UpdateDetails");
 			Guard.ForNullOrEmptyString(title, "title");
 
 			Title = title;
 			Level = level;
 			Dates = new TournamentDates(startDate, endDate);
+		}
+
+		public void AddEvent(Event tennisEvent)
+		{
+			GuardForActionInCorrectState(TournamentState.BeingDefined, "AddEvent");
+			GuardForDuplicateEventType(tennisEvent.EventType);
+
+			_events.Add(tennisEvent.EventType, tennisEvent);
+		}
+
+		public void RemoveEvent(EventType eventType)
+		{
+			GuardForActionInCorrectState(TournamentState.BeingDefined, "RemoveEvent");
+			GuardForMissingEventType(eventType);
+
+			_events.Remove(eventType);
+		}
+
+		public void ClearEvents()
+		{
+			GuardForActionInCorrectState(TournamentState.BeingDefined, "ClearEvents");
+
+			_events.Clear();
+		}
+
+		public void SetEvents(IEnumerable<Event> events)
+		{
+			GuardForActionInCorrectState(TournamentState.BeingDefined, "SetEvents");
+
+			_events.Clear();
+
+			try
+			{
+				foreach (var tennisEvent in events)
+				{
+					GuardForDuplicateEventType(tennisEvent.EventType);
+					_events.Add(tennisEvent.EventType, tennisEvent);
+				}
+			}
+			catch (Exception)
+			{
+				_events.Clear();
+				throw;
+			}
+		}
+
+		private void GuardForActionInCorrectState(TournamentState state, string action)
+		{
+			if (State != state)
+			{
+				throw new Exception($"Action {action} not allowed for a tournament if the state {state}");
+			}
+		}
+
+		private void GuardForDuplicateEventType(EventType eventType)
+		{
+			if (_events.ContainsKey(eventType))
+			{
+				throw new Exception($"Tournament already has an event of type {eventType}");
+			}
+		}
+
+		private void GuardForMissingEventType(EventType eventType)
+		{
+			if (!_events.ContainsKey(eventType))
+			{
+				throw new Exception($"Tournament does not have an event of type {eventType}");
+			}
 		}
 	}
 }
