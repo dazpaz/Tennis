1077b253e34921b031fd9a94cc4aa779f384b8c3
Darren Powell <darren.powell@hotmail.co.uk>
2021-10-31 17:45:37 +0000

    Add CompetitorId value object

---
+++ src/TournamentManagement/TournamentManagement.Domain.UnitTests/CompetitorIdTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace TournamentManagement.Domain.UnitTests
+{
+	public class CompetitorIdTests
+	{
+		[Fact]
+		public void CanCreateCompetitorIdWithNoParameters()
+		{
+			var competitorId = new CompetitorId();
+
+			competitorId.Id.Should().NotBe(Guid.Empty);
+		}
+
+		[Fact]
+		public void CanCreateCompetitorIdUsingASpecifiedGuid()
+		{
+			var id = Guid.NewGuid();
+			var competitorId = new CompetitorId(id);
+
+			competitorId.Id.Should().Be(id);
+		}
+
+		[Fact]
+		public void CannotCreateCompetitorIdUsingAnEmptyGuid()
+		{
+			Action act = () => new CompetitorId(Guid.Empty);
+
+			act.Should()
+				.Throw<ArgumentException>()
+				.WithMessage("Guid cannot have empty value (Parameter 'id')");
+		}
+	}
+}
---
+++ src/TournamentManagement/TournamentManagement.Domain/CompetitorId.cs
@@ -0,0 +1,21 @@
+﻿using DomainDesign.Common;
+using System;
+
+namespace TournamentManagement.Domain
+{
+	public class CompetitorId : ValueObject<CompetitorId>
+	{
+		public readonly Guid Id;
+
+		public CompetitorId()
+		{
+			Id = Guid.NewGuid();
+		}
+
+		public CompetitorId(Guid id)
+		{
+			Guard.ForGuidIsNotEmpty(id, "id");
+			Id = id;
+		}
+	}
+}
