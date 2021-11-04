5e3c161c269b786655535d182c3b36a53afe23d7
Darren Powell <darren.powell@hotmail.co.uk>
2021-11-04 21:45:55 +0000

    Add SetScore Value Object

---
+++ src/TournamentManagement/TournamentManagement.Domain/SetScore.cs
@@ -0,0 +1,19 @@
+﻿using DomainDesign.Common;
+
+namespace TournamentManagement.Domain
+{
+	public class SetScore : ValueObject<SetScore>
+	{
+		public int GamesOne { get; }
+		public int GamesTwo { get; }
+
+		public SetScore(int gamesOne, int gamesTwo)
+		{
+			Guard.ForIntegerOutOfRange(gamesOne, 0, 99, "gamesOne");
+			Guard.ForIntegerOutOfRange(gamesTwo, 0, 99, "gamesTwo");
+
+			GamesOne = gamesOne;
+			GamesTwo = gamesTwo;
+		}
+	}
+}
