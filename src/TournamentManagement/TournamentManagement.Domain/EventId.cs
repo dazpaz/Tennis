b7ec65a901791a2e448466a46b6e4fb2671c67f3
Darren Powell <darren.powell@hotmail.co.uk>
2021-11-06 23:23:19 +0000

    Add EventId Value Object

---
+++ src/TournamentManagement/TournamentManagement.Domain/EventId.cs
@@ -0,0 +1,21 @@
+﻿using DomainDesign.Common;
+using System;
+
+namespace TournamentManagement.Domain
+{
+	public class EventId : ValueObject<EventId>
+	{
+		public readonly Guid Id;
+
+		public EventId()
+		{
+			Id = Guid.NewGuid();
+		}
+
+		public EventId(Guid id)
+		{
+			Guard.ForGuidIsNotEmpty(id, "id");
+			Id = id;
+		}
+	}
+}
