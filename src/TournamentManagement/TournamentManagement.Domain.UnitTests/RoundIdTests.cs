ce138c6483b4a0986c238aa7337dc31f403733d6
Darren Powell <darren.powell@hotmail.co.uk>
2021-11-07 22:02:27 +0000

    Add round entity (#17)
    * Add RoundId value object
    
    * Add Round Entity to the Domain Model

---
+++ src/TournamentManagement/TournamentManagement.Domain.UnitTests/RoundIdTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace TournamentManagement.Domain.UnitTests
+{
+	public class RoundIdTests
+	{
+		[Fact]
+		public void CanCreateRoundIdWithNoParameters()
+		{
+			var roundId = new RoundId();
+
+			roundId.Id.Should().NotBe(Guid.Empty);
+		}
+
+		[Fact]
+		public void CanCreateTournamentIdUsingASpecifiedGuid()
+		{
+			var id = Guid.NewGuid();
+			var roundId = new RoundId(id);
+
+			roundId.Id.Should().Be(id);
+		}
+
+		[Fact]
+		public void CannotCreateTournamentIdUsingAnEmptyGuid()
+		{
+			Action act = () => new RoundId(Guid.Empty);
+
+			act.Should()
+				.Throw<ArgumentException>()
+				.WithMessage("Guid cannot have empty value (Parameter 'id')");
+		}
+	}
+}
---
+++ src/TournamentManagement/TournamentManagement.Domain/RoundId.cs
@@ -0,0 +1,21 @@
+﻿using DomainDesign.Common;
+using System;
+
+namespace TournamentManagement.Domain
+{
+	public class RoundId : ValueObject<RoundId>
+	{
+		public readonly Guid Id;
+
+		public RoundId()
+		{
+			Id = Guid.NewGuid();
+		}
+
+		public RoundId(Guid id)
+		{
+			Guard.ForGuidIsNotEmpty(id, "id");
+			Id = id;
+		}
+	}
+}
