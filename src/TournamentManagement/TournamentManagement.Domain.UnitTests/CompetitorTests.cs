8dce47e1b093a20f6179d5f1b76389072e74c415
Darren Powell <darren.powell@hotmail.co.uk>
2021-11-08 23:19:38 +0000

    Add Competitor entity to the Domain

---
+++ src/TournamentManagement/TournamentManagement.Domain.UnitTests/CompetitorTests.cs
@@ -0,0 +1,83 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace TournamentManagement.Domain.UnitTests
+{
+	public class CompetitorTests
+	{
+		[Fact]
+		public void CanUseFactoryMethodToCreateCompetitorForASinglesEventAndItIsCreatedCorrectly()
+		{
+			var tournamentId = new TournamentId();
+			var eventEntryId = Guid.NewGuid();
+			var competitor = Competitor.Create(tournamentId, EventType.MensSingles, eventEntryId, 1,
+				new List<string>() { "Steve Serve" });
+
+			competitor.Id.Id.Should().NotBe(Guid.Empty);
+			competitor.TournamentId.Should().Be(tournamentId);
+			competitor.EventType.Should().Be(EventType.MensSingles);
+			competitor.Seeding.Should().Be(1);
+			competitor.PlayerNames.Count.Should().Be(1);
+			competitor.PlayerNames[0].Should().Be("Steve Serve");
+		}
+
+		[Fact]
+		public void CanUseFactoryMethodToCreateCompetitorForADoublesEventAndItIsCreatedCorrectly()
+		{
+			var tournamentId = new TournamentId();
+			var eventEntryId = Guid.NewGuid();
+			var competitor = Competitor.Create(tournamentId, EventType.MensDoubles, eventEntryId, 1,
+				new List<string>() { "Steve Serve", "Vernon Volley" });
+
+			competitor.Id.Id.Should().NotBe(Guid.Empty);
+			competitor.TournamentId.Should().Be(tournamentId);
+			competitor.EventType.Should().Be(EventType.MensDoubles);
+			competitor.Seeding.Should().Be(1);
+			competitor.PlayerNames.Count.Should().Be(2);
+			competitor.PlayerNames[0].Should().Be("Steve Serve");
+			competitor.PlayerNames[1].Should().Be("Vernon Volley");
+		}
+
+		[Theory]
+		[InlineData(-1)]
+		[InlineData(33)]
+		public void CannotCreateCompetitorWithInvalidSeeding(int seeding)
+		{
+			var tournamentId = new TournamentId();
+			var eventEntryId = Guid.NewGuid();
+			
+			Action act = () => Competitor.Create(tournamentId, EventType.MensSingles, eventEntryId, seeding,
+				new List<string>() { "Steve Serve" });
+
+			act.Should()
+				.Throw<Exception>()
+				.WithMessage($"Value {seeding} must be between 0 and 32 (Parameter 'seeding')");
+		}
+
+		[Theory]
+		[InlineData(EventType.MensSingles, 2)]
+		[InlineData(EventType.WomensSingles, 2)]
+		[InlineData(EventType.MensDoubles, 1)]
+		[InlineData(EventType.WomensDoubles, 1)]
+		[InlineData(EventType.MixedDoubles, 1)]
+		public void CannotCreateCompetitorWithWrongNumberOfPlayers(EventType eventType, int numberOfPlayers)
+		{
+			var tournamentId = new TournamentId();
+			var eventEntryId = Guid.NewGuid();
+			var playersNames = new List<string>();
+			for (int i = 0; i < numberOfPlayers; i++)
+			{
+				playersNames.Add("Player");
+			}
+
+			Action act = () => Competitor.Create(tournamentId, eventType, eventEntryId, 1,
+				playersNames);
+
+			act.Should()
+				.Throw<Exception>()
+				.WithMessage("Wrong number of players for this event type");
+		}
+	}
+}
---
+++ src/TournamentManagement/TournamentManagement.Domain/Competitor.cs
@@ -0,0 +1,53 @@
+﻿using DomainDesign.Common;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentManagement.Domain
+{
+	public class Competitor : Entity<CompetitorId>
+	{
+		public TournamentId TournamentId { get; private set; }
+		public EventType EventType { get; private set; }
+		public Guid EventEntryId { get; private set; }
+		public int Seeding { get; private set; }
+		public ReadOnlyCollection<string> PlayerNames { get; private set; }
+
+		private IList<string> _playersNames { get; set; }
+
+		private Competitor(CompetitorId id, ICollection<string> playersNames) : base(id)
+		{
+			_playersNames = new List<string>(playersNames);
+			PlayerNames = new ReadOnlyCollection<string>(_playersNames);
+		}
+
+		public static Competitor Create(TournamentId tournamentId, EventType eventType,
+			Guid eventEntryId, int seeding, ICollection<string> playersNames)
+		{
+			Guard.ForIntegerOutOfRange(seeding, 0, 32, "seeding");
+			GuardAgainstWrongNumberOfPlayers(eventType, playersNames.Count);
+
+			var competitor = new Competitor(new CompetitorId(), playersNames)
+			{
+				TournamentId = tournamentId,
+				EventType = eventType,
+				EventEntryId = eventEntryId,
+				Seeding = seeding,
+			};
+
+			return competitor;
+		}
+
+		private static void GuardAgainstWrongNumberOfPlayers(EventType eventType, int playerCount)
+		{
+			var expectedPlayerCount = Event.IsSinglesEvent(eventType) ? 1 : 2;
+			if (playerCount != expectedPlayerCount)
+			{
+				throw new Exception("Wrong number of players for this event type");
+			}
+		}
+	}
+}
