3658d9833e6bc392fabffdd9b64655a76d949491
Darren Powell <darren.powell@hotmail.co.uk>
2021-11-13 20:48:03 +0000

    Add Player Id value object

---
+++ src/TournamentManagement/TournamentManagement.Domain.UnitTests/PlayerIdTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace TournamentManagement.Domain.UnitTests
+{
+	public class PlayerIdTests
+	{
+		[Fact]
+		public void CanCreatePlayerIdWithNoParameters()
+		{
+			var playerId = new PlayerId();
+
+			playerId.Id.Should().NotBe(Guid.Empty);
+		}
+
+		[Fact]
+		public void CanCreatePlayerIdUsingASpecifiedGuid()
+		{
+			var id = Guid.NewGuid();
+			var playerId = new PlayerId(id);
+
+			playerId.Id.Should().Be(id);
+		}
+
+		[Fact]
+		public void CannotCreatePlayerIdUsingAnEmptyGuid()
+		{
+			Action act = () => new PlayerId(Guid.Empty);
+
+			act.Should()
+				.Throw<ArgumentException>()
+				.WithMessage("Guid cannot have empty value (Parameter 'id')");
+		}
+	}
+}
