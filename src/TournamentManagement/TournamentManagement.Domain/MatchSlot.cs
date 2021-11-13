4b6e8ebf7c8e75827f26742cf0fdda96bb371b6d
Darren Powell <darren.powell@hotmail.co.uk>
2021-11-13 20:17:36 +0000

    Add Match Slot value object

---
+++ src/TournamentManagement/TournamentManagement.Domain/MatchSlot.cs
@@ -0,0 +1,27 @@
+﻿using DomainDesign.Common;
+using System;
+
+namespace TournamentManagement.Domain
+{
+	public class MatchSlot : ValueObject<MatchSlot>
+	{
+		public MatchId MatchId { get; }
+		public int Slot { get; }
+
+		public MatchSlot(MatchId matchId, int slot)
+		{
+			GuardAgainstInvalidCompetitorSlot(slot);
+
+			MatchId = matchId;
+			Slot = slot;
+		}
+
+		private static void GuardAgainstInvalidCompetitorSlot(int slot)
+		{
+			if (slot < 1 || slot > 2)
+			{
+				throw new Exception($"Invalid Competitor Position {slot}, it must be 1 or 2");
+			}
+		}
+	}
+}
