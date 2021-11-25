371e40ccaaf42a38de28cfc3dedf8f09204d90bd
Darren Powell <darren.powell@hotmail.co.uk>
2021-11-25 21:30:39 +0000

    Add venue (#29)
    * Create basic Venue entity
    
    * Add Court Entity
    
    * Add collection of Courts to a Venue

---
+++ src/TournamentManagement/TournamentManagement.Domain.UnitTests/VenueIdTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace TournamentManagement.Domain.UnitTests
+{
+	public class VenueIdTests
+	{
+		[Fact]
+		public void CanCreateVenueIdWithNoParameters()
+		{
+			var venueId = new VenueId();
+
+			venueId.Id.Should().NotBe(Guid.Empty);
+		}
+
+		[Fact]
+		public void CanCreateVenueIdUsingASpecifiedGuid()
+		{
+			var id = Guid.NewGuid();
+			var venueId = new VenueId(id);
+
+			venueId.Id.Should().Be(id);
+		}
+
+		[Fact]
+		public void CannotCreateVenueIdUsingAnEmptyGuid()
+		{
+			Action act = () => new VenueId(Guid.Empty);
+
+			act.Should()
+				.Throw<ArgumentException>()
+				.WithMessage("Guid cannot have empty value (Parameter 'id')");
+		}
+	}
+}
---
+++ src/TournamentManagement/TournamentManagement.Domain/Venue.cs
@@ -0,0 +1,59 @@
+﻿using DomainDesign.Common;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace TournamentManagement.Domain
+{
+	public class Venue : Entity<VenueId>
+	{
+		public string Name { get; private set; }
+		public Surface Surface { get; private set; }
+		public ReadOnlyCollection<Court> Courts { get; }
+
+		private readonly IList<Court> _courts;
+
+		private Venue(VenueId id) : base(id)
+		{
+			_courts = new List<Court>();
+			Courts = new ReadOnlyCollection<Court>(_courts);
+		}
+
+		public static Venue Create(string name, Surface surface)
+		{
+			Guard.AgainstNullOrEmptyString(name, nameof(name));
+
+			var venue = new Venue(new VenueId())
+			{
+				Name = name,
+				Surface = surface
+			};
+
+			return venue;
+		}
+
+		public void AddCourt(Court court)
+		{
+			GuardAgainstDuplicateCourtName(court.Name);
+			_courts.Add(court);
+		}
+
+		public void RemoveCourt(CourtId id)
+		{
+			var court = _courts.FirstOrDefault(c => c.Id == id);
+			if (court != null)
+			{
+				_courts.Remove(court);
+			}
+		}
+
+		private void GuardAgainstDuplicateCourtName(string name)
+		{
+			if (_courts.Any(c => c.Name == name))
+			{
+				throw new Exception($"Cannot add court to venue, court name already exists: {name}");
+			}
+		}
+	}
+}
