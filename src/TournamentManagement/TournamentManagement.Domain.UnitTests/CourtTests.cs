28bcb570b34471e30fb0ebce1c20551fd8727aae
Darren Powell <darren.powell@hotmail.co.uk>
2021-11-25 20:10:45 +0000

    Add Court Entity

---
+++ src/TournamentManagement/TournamentManagement.Domain.UnitTests/CourtTests.cs
@@ -0,0 +1,101 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace TournamentManagement.Domain.UnitTests
+{
+	public class CourtTests
+	{
+		[Fact]
+		public void CanUseFactoryMethodToCreateCourtAndItIsCreatedCorrectly()
+		{
+			var court = Court.Create("Centre Court", 14979);
+
+			court.Id.Id.Should().NotBe(Guid.Empty);
+			court.Name.Should().Be("Centre Court");
+			court.Capacity.Should().Be(14979);
+		}
+
+		[Theory]
+		[InlineData(null)]
+		[InlineData("")]
+		public void CannotCreateACourtWithEmptyName(string name)
+		{
+			Action act = () => Court.Create(name, 100);
+
+			act.Should()
+				.Throw<ArgumentException>()
+				.WithMessage("Value can not be null or empty string (Parameter 'name')");
+		}
+
+		[Theory]
+		[InlineData(0)]
+		[InlineData(20000)]
+		public void CanCreateACourtWithCapacityValuesAtTheLimitsOfTheValidRange(int capacity)
+		{
+			var court = Court.Create("Centre Court", capacity);
+
+			court.Name.Should().Be("Centre Court");
+			court.Capacity.Should().Be(capacity);
+		}
+
+		[Theory]
+		[InlineData(-1)]
+		[InlineData(20001)]
+		public void CannotCreateCourtWithCapacityValuesOutsideTheValidRange(int capacity)
+		{
+			Action act = () => Court.Create("Centre Court", capacity);
+
+			act.Should()
+				.Throw<ArgumentException>();
+		}
+		
+		[Fact]
+		public void CanRenameACourt()
+		{
+			var court = Court.Create("Centre Court", 14979);
+
+			court.RenameCourt("Murray Court");
+
+			court.Name.Should().Be("Murray Court");
+		}
+
+		[Fact]
+		public void CanUpdateTheCapacityOfACourt()
+		{
+			var court = Court.Create("Court 4", 100);
+
+			court.UpdateCapacity(200);
+
+			
+			court.Capacity.Should().Be(200);
+		}
+
+		[Theory]
+		[InlineData(null)]
+		[InlineData("")]
+		public void CannotRenameACourtWithEmptyName(string newName)
+		{
+			var court = Court.Create("Court 4", 100);
+
+			Action act = () => court.RenameCourt(newName);
+
+			act.Should()
+				.Throw<ArgumentException>()
+				.WithMessage("Value can not be null or empty string (Parameter 'newName')");
+		}
+
+		[Theory]
+		[InlineData(-1)]
+		[InlineData(20001)]
+		public void CannotUpdateCapacityWithValueOutsideTheValidRange(int capacity)
+		{
+			var court = Court.Create("Court 4", 100);
+
+			Action act = () => court.UpdateCapacity(capacity);
+
+			act.Should()
+				.Throw<ArgumentException>();
+		}
+	}
+}
---
+++ src/TournamentManagement/TournamentManagement.Domain/Court.cs
@@ -0,0 +1,40 @@
+﻿using DomainDesign.Common;
+
+namespace TournamentManagement.Domain
+{
+	public class Court : Entity<CourtId>
+	{
+		public string Name { get; private set; }
+		public int Capacity { get; private set; } 
+
+		private Court(CourtId id) : base(id)
+		{
+		}
+
+		public static Court Create(string name, int capacity)
+		{
+			Guard.AgainstNullOrEmptyString(name, nameof(name));
+			Guard.AgainstIntegerOutOfRange(capacity, 0, 20000, nameof(capacity));
+
+			var court = new Court(new CourtId())
+			{
+				Name = name,
+				Capacity = capacity
+			};
+
+			return court;
+		}
+
+		public void UpdateCapacity(int newCapacity)
+		{
+			Guard.AgainstIntegerOutOfRange(newCapacity, 0, 20000, nameof(newCapacity));
+			Capacity = newCapacity;
+		}
+
+		public void RenameCourt(string newName)
+		{
+			Guard.AgainstNullOrEmptyString(newName, nameof(newName));
+			Name = newName;
+		}
+	}
+}
