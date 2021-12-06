9a36f5d4dd2e3dbd74cf7e7b68ea59eca68212c0
Darren Powell <darren.powell@hotmail.co.uk>
2021-12-06 23:32:24 +0000

    Add more actions to Tournament Controller

--- src/TournamentManagement/TournamentManagement.WebApi/Controllers/TournamentController.cs
+++ src/TournamentManagement/TournamentManagement.WebApi/Controllers/TournamentController.cs
@@ -8,11 +8,20 @@ namespace TournamentManagement.WebApi.Controllers
 	[ApiController]
 	public class TournamentController : ControllerBase
 	{
-		[HttpGet]
-		[Route("{id}")]
+		[HttpGet()]
+		public IActionResult GetTournaments()
+		{
+			// call to query tournament handler to get the tournaments
+			// convert to list of dtos
+			// return ok with the list
+
+			return Ok("List of tournaments goes here");
+		}
+
+		[HttpGet("{id}")]
 		public IActionResult GetTournament(Guid id)
 		{
-			// call to query handler to get the tournament
+			// call to query tournament handler to get the tournament by id
 			// if not found - return 404
 			// if found convert to dto and return the summary details
 
@@ -28,22 +37,75 @@ namespace TournamentManagement.WebApi.Controllers
 		}
 
 		[HttpPost]
-		public IActionResult Create([FromBody] CreateTournamentDto tournament)
+		public IActionResult CreateTournament([FromBody] CreateTournamentDto tournament)
 		{
+			// if not valid request return bad request
 			// call to create tournament command to create the tournament
 			// if fails return 400 - you were bad
 			// if success return created with location (question - sould we rturn a body)
 
-			return CreatedAtAction(nameof(GetTournament), new { id = Guid.NewGuid() }, tournament);
+			return CreatedAtAction(nameof(GetTournament), new { id = Guid.NewGuid() }, null);
 		}
 
-		[HttpPut]
-		[Route("{id}")]
-		public IActionResult Update(Guid id, [FromBody] UpdateTournamentDto tournament)
+		[HttpPut("{id}")]
+		public IActionResult UpdateTournament(Guid id, [FromBody] UpdateTournamentDto tournament)
 		{
 			// call to update tournament command to update the tournament
+			// if tournament does not exist - 404
 			// if fails return 400 - you were bad
-			// if success return created with location and 
+			// if success return no content
+
+			return NoContent();
+		}
+
+		[HttpGet("{id}/events")]
+		public IActionResult GetEvents(Guid id)
+		{
+			// call query to get all events for this tournament
+
+			return Ok("List of events goes here");
+		}
+
+
+		[HttpGet("{id}/events/{eventType}")]
+		public IActionResult GetEvent(Guid id, string eventType)
+		{
+			// call query
+			// if not found - 404
+			// iff error 400
+			// otherwse return 200 and the event
+
+			return Ok("Event goes here");
+		}
+
+		[HttpPost("{id}/addevent")]
+		public IActionResult AddEvent(Guid id, [FromBody] CreateEventDto tennisEvent)
+		{
+			// call command to add the event
+			// if tournament does not exist - return 404
+			// if fails return a 400 - you were bad
+			// if success return created
+
+			return CreatedAtAction(nameof(GetEvent), new { id, tennisEvent.EventType }, null);
+		}
+
+		[HttpDelete("{id}/removeevent/{eventType}")]
+		public IActionResult RemoveEvent(Guid id, string eventType)
+		{
+			// call command to add the event
+			// if tournament does not exist - return 404
+			// if fails return a 400 - you were bad
+			// if success return created
+
+			return Ok();
+		}
+
+		[HttpPost("{id}/openforentries")]
+		public IActionResult OpenForEntries(Guid id)
+		{
+			// call into command to do the open command
+			// if id does not extist - 404
+			// any other error 400
 
 			return NoContent();
 		}
---
+++ src/TournamentManagement/TournamentManagement.WebApi/Dto/CreateEventDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TournamentManagement.WebApi.Dto
+{
+	public class CreateEventDto
+	{
+		public int EventType { get; set; }
+		public int EntrantsLimit { get; set; }
+		public int NumberOfSeeds { get; set; }
+		public int NumberOfSets { get; set; }
+		public int FinalSetType { get; set; }
+	}
+}
