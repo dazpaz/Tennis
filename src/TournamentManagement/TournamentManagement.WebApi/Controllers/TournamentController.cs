93919368c58d74b1ccdc427eee3fd06f6f95c579
Darren Powell <darren.powell@hotmail.co.uk>
2021-12-09 22:53:52 +0000

    Create tournament (#36)
    * Create vanilla Web API for Tournament Management
    
    * Add dummy Tournament Controller with Get, Pot and Push
    
    * Add more actions to Tournament Controller
    
    * Change to Add and Amend Tournament from Create and Update
    
    * Add start of the Add Tournament Command

---
+++ src/TournamentManagement/TournamentManagement.WebApi/Controllers/TournamentController.cs
@@ -0,0 +1,44 @@
+﻿using CSharpFunctionalExtensions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using TournamentManagement.Application;
+using TournamentManagement.WebApi.Dto;
+
+namespace TournamentManagement.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TournamentController : ControllerBase
+	{
+		[HttpPost]
+		public IActionResult AddTournament([FromBody] AddTournamentDto tournament)
+		{
+			var command = MapDtoToAddTournamentCommand(tournament);
+
+			var handler = new AddTournamentCommandHandler(null);
+			Result<Guid> result = handler.Handle(command);
+
+			return result.IsSuccess
+				? CreatedAtAction(nameof(GetTournament), new { id = result.Value }, null)
+				: BadRequest(result.Error);
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult GetTournament(Guid id)
+		{
+			return Ok($"Tournament {id} to ho here");
+		}
+
+		private static AddTournamentCommand MapDtoToAddTournamentCommand(AddTournamentDto tournament)
+		{
+			return new AddTournamentCommand()
+			{
+				Title = tournament.Title,
+				TournamentLevel = tournament.TournamentLevel,
+				StartDate = tournament.StartDate,
+				EndDate = tournament.EndDate,
+				VenueId = tournament.VenueId
+			};
+		}
+	}
+}
