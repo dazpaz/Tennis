9c70361f74a622ef571b2fa7803fab911c508041
Darren Powell <darren.powell@hotmail.co.uk>
2021-12-19 19:30:32 +0000

    Add Register Player Command

---
+++ src/TournamentManagement/TournamentManagement.Domain/PlayerAggregate/Repository/IPlayerRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentManagement.Domain.PlayerAggregate.Repository
+{
+	public interface IPlayerRepository
+	{
+	}
+}
