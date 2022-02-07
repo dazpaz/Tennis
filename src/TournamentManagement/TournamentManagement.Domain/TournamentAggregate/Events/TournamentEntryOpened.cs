b46e83e03b66e93a12fa121a998c3e7207ce07bf
Darren Powell <darren.powell@hotmail.co.uk>
2022-02-07 23:26:14 +0000

    Add two Domain Events for the Tournament aggregate

---
+++ src/TournamentManagement/TournamentManagement.Domain/TournamentAggregate/Events/TournamentEntryOpened.cs
@@ -0,0 +1,17 @@
+﻿using DomainDesign.Common;
+using System.Collections.Generic;
+
+namespace TournamentManagement.Domain.TournamentAggregate.Events
+{
+	public sealed class TournamentEntryOpened : IDomainEvent
+	{
+		public TournamentId TournamentId { get; }
+		public IEnumerable<EventType> EventTypes { get; }
+
+		public TournamentEntryOpened(TournamentId tournamentId, IEnumerable<EventType> eventTypes)
+		{
+			TournamentId = tournamentId;
+			EventTypes = eventTypes;
+		}
+	}
+}
