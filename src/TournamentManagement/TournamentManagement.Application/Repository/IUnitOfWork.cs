1627e8b63bb568d18881b9ee1dbd56ab6de230c3
Darren Powell <darren.powell@hotmail.co.uk>
2022-02-10 22:36:54 +0000

    Unit of work (#58)
    * Add simple Unit of Work to Tournament Management
    
    * Change Program.cs to use the Unit of Work

---
+++ src/TournamentManagement/TournamentManagement.Application/Repository/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace TournamentManagement.Application.Repository
+{
+	public interface IUnitOfWork
+	{
+		ICompetitorRepository CompetitorRepository { get; }
+		IPlayerRepository PlayerRepository { get; }
+		IRoundRepository RoundRepository { get; }
+		ITournamentRepository TournamentRepository { get; }
+		IVenueRepository VenueRepository { get; }
+		int SaveChanges();
+	}
+}
