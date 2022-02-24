d84085f7e85425c2f2430aaf468a4527fdf2308b
Darren Powell <darren.powell@hotmail.co.uk>
2022-02-24 00:14:19 +0000

    Add the Add Event Command

---
+++ src/TournamentManagement/TournamentManagement.Contract/AddEventDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TournamentManagement.Contract
+{
+	public class AddEventDto
+	{
+		public EventType EventType { get; set; }
+		public int EntrantsLimit { get; set; }
+		public int NumberOfSeeds { get; set; }
+		public int NumberOfSets { get; set; }
+		public SetType FinalSetType { get; set; }
+	}
+}
