6ba288b9cf9739a6288572e5b24ec847fc28ff94
Darren Powell <darren.powell@hotmail.co.uk>
2022-03-12 09:31:11 +0000

    Replace individual repositories with generic one

---
+++ src/TournamentManagement/TournamentManagement.Application/Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using DomainDesign.Common;
+
+namespace TournamentManagement.Application.Repository
+{
+	public interface IRepository<TAggregate, TKey>
+		where TAggregate : class, IAggregateRoot
+		where TKey : class // constraint to EntityId
+	{
+		TAggregate GetById(TKey id);
+		void Add(TAggregate player);
+	}
+}
---
+++ src/TournamentManagement/TournamentManagement.Data/Repository/Repository.cs
@@ -0,0 +1,29 @@
+﻿using DomainDesign.Common;
+using Microsoft.EntityFrameworkCore;
+using TournamentManagement.Application.Repository;
+
+namespace TournamentManagement.Data.Repository
+{
+	public class Repository<TAggregate, TKey> : IRepository<TAggregate, TKey>
+		where TAggregate : class, IAggregateRoot
+		where TKey : class
+	{
+		private readonly DbSet<TAggregate> _dbSet;
+
+		public Repository(TournamentManagementDbContext context)
+		{
+			_dbSet = context.Set<TAggregate>();
+		}
+
+		public void Add(TAggregate entity)
+		{
+			_dbSet.Add(entity);
+		}
+
+		public TAggregate GetById(TKey id)
+		{
+			var round = _dbSet.Find(id);
+			return round;
+		}
+	}
+}
