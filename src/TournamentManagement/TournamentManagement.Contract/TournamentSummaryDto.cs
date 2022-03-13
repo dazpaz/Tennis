aa84440579a87ae911ea5d6e8ee4de4d5d2a23eb
Darren Powell <darren.powell@hotmail.co.uk>
2022-03-13 21:38:58 +0000

    Small changes to the TournamentSummaryDto

--- src/TournamentManagement/TournamentManagement.Contract/TournamentSummaryDto.cs
+++ src/TournamentManagement/TournamentManagement.Contract/TournamentSummaryDto.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TournamentManagement.Common;
 
 namespace TournamentManagement.Contract
 {
@@ -6,10 +7,10 @@ namespace TournamentManagement.Contract
 	{
 		public Guid Id { get; set; }
 		public string Title { get; set; }
-		public string TournamentLevel { get; set; }
+		public TournamentLevel Level { get; set; }
 		public DateTime StartDate { get; set; }
 		public DateTime EndDate { get; set; }
-		public string State { get; set; }
+		public TournamentState State { get; set; }
 		public string VenueName { get; set; }
 		public int NumberOfEvents { get; set; }
 	}
