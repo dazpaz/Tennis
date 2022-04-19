5b3a7573e0cf25c387c6f07070380db8d9615a5f
Darren Powell <darren.powell@hotmail.co.uk>
2022-04-19 22:13:35 +0100

    Create minimal Identity Server

---
+++ src/IdentityServer/Config.cs
@@ -0,0 +1,35 @@
+﻿using Duende.IdentityServer.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+	public static class Config
+	{
+		public static IEnumerable<ApiScope> ApiScopes =>
+			new List<ApiScope>
+			{
+				new ApiScope("tournamentManagement", "Tournament Management API")
+			};
+
+		public static IEnumerable<Client> Clients =>
+			new List<Client>
+			{
+				new Client
+				{
+					ClientId = "api-tests",
+
+					// no interactive user, use the clientid/secret for authentication
+					AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+					// secret for authentication
+					ClientSecrets =
+					{
+						new Secret("api-tests-secret".Sha256())
+					},
+
+					// scopes that client has access to
+					AllowedScopes = { "tournamentManagement" }
+				}
+			};
+	}
+}
