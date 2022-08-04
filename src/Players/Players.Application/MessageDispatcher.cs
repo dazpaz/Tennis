f9d3deca062700f9cc35eee0468e53439d344776
Darren Powell <darren.powell@hotmail.co.uk>
2022-08-04 22:12:45 +0100

    Add Message Dispatcher to Player Application layer

---
+++ src/Players/Players.Application/MessageDispatcher.cs
@@ -0,0 +1,53 @@
+﻿using CSharpFunctionalExtensions;
+using DomainDesign.Common;
+
+namespace Players.Application;
+
+public sealed class MessageDispatcher
+{
+	private readonly IServiceProvider _provider;
+
+	public MessageDispatcher(IServiceProvider provider)
+	{
+		_provider = provider;
+	}
+
+	public Result Dispatch(ICommand command)
+	{
+		Type type = typeof(ICommandHandler<>);
+		Type[] typeArgs = { command.GetType() };
+		Type handlerType = type.MakeGenericType(typeArgs);
+
+		dynamic? handler = _provider.GetService(handlerType);
+
+		return handler == null
+			? Result.Failure($"No handler defined for {command.GetType().Name} command")
+			: handler.Handle((dynamic)command);
+	}
+
+	public Result<TResult> Dispatch<TResult>(ICommand command)
+	{
+		Type type = typeof(ICommandHandler<,>);
+		Type[] typeArgs = { command.GetType(), typeof(TResult) };
+		Type handlerType = type.MakeGenericType(typeArgs);
+
+		dynamic? handler = _provider.GetService(handlerType);
+
+		return handler == null
+			? Result.Failure<TResult>($"No handler defined for {command.GetType().Name} command")
+			: handler.Handle((dynamic)command);
+	}
+
+	public Result<TResult> Dispatch<TResult>(IQuery<TResult> query)
+	{
+		Type type = typeof(IQueryHandler<,>);
+		Type[] typeArgs = { query.GetType(), typeof(TResult) };
+		Type handlerType = type.MakeGenericType(typeArgs);
+
+		dynamic? handler = _provider.GetService(handlerType);
+
+		return handler == null
+			? Result.Failure<TResult>($"No handler defined for {query.GetType().Name} query")
+			: handler.Handle((dynamic)query);
+	}
+}
