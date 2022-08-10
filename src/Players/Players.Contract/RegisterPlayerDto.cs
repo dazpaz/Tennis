600c8710187c30f98953855fc911da8154fcff0f
Darren Powell <darren.powell@hotmail.co.uk>
2022-08-10 21:11:36 +0100

    Add Register Player to the domain
    Minimal - no validation

--- src/Players/Players.Contract/RegisterPlayerDto.cs
+++ src/Players/Players.Contract/RegisterPlayerDto.cs
@@ -6,6 +6,7 @@ public class RegisterPlayerDto
 {
 	public string FirstName { get; set; } = string.Empty;
 	public string LastName { get; set; } = string.Empty;
+	public Gender Gender { get; set; }
 	public DateOnly DateOfBirth { get; set; }
 	public Plays Plays { get; set; }
 	public int Height { get; set; }
---
+++ src/Players/Players.Domain/PlayerAggregate/Player.cs
@@ -0,0 +1,52 @@
+﻿using DomainDesign.Common;
+using Players.Common;
+
+#nullable disable
+
+namespace Players.Domain.PlayerAggregate;
+
+public class Player : AggregateRoot<PlayerId>
+{
+	public string FirstName { get; private set; }
+	public string LastName { get; private set; }
+	public string FullName { get; private set; }
+	public Gender Gender { get; private set; }
+	public DateOnly DateOfBirth { get; private set; }
+	public Plays Plays { get; private set; }
+	public int Height { get; private set; }
+	public string Country { get; private set; }
+	public int SinglesRank { get; private set; }
+	public int DoublesRank { get; private set; }
+	public int SinglesRankingPoints { get; private set; }
+	public int DoublesRankingPoints { get; private set; }
+
+	protected Player()
+	{
+	}
+
+	private Player(PlayerId id) : base(id)
+	{
+	}
+
+	public static Player Register(string firstName, string lastName, Gender gender,
+		DateOnly dateOfBirth, Plays plays, int height, string country)
+	{
+		var player = new Player(new PlayerId())
+		{
+			FirstName = firstName,
+			LastName = lastName,
+			FullName = $"{firstName} {lastName}",
+			Gender = gender,
+			DateOfBirth = dateOfBirth,
+			Plays = plays,
+			Height = height,
+			Country = country,
+			SinglesRank = 999,
+			DoublesRank = 999,
+			SinglesRankingPoints = 0,
+			DoublesRankingPoints = 0
+		};
+
+		return player;
+	}
+}
