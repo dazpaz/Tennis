bcfb52c2da6cfd5e26da704da0a1c31307edd855
Darren Powell <darren.powell@hotmail.co.uk>
2022-08-18 22:00:09 +0100

    Get player details and get player summary list queries (#90)
    * Add Get PlayerDetails Query
    
    * Add Get Player summary list query

---
+++ src/Players/Players.Query/GetPlayerSummaryList.cs
@@ -0,0 +1,41 @@
+﻿using Cqrs.Common.Data;
+using Dapper;
+using DomainDesign.Common;
+using Microsoft.Data.SqlClient;
+using Players.Contract;
+
+#nullable disable
+
+namespace Players.Query;
+
+public sealed class GetPlayerSummaryList : IQuery<IList<PlayerSummaryDto>>
+{
+}
+
+public sealed class GetPlayerSummaryListHandler
+	: IQueryHandler<GetPlayerSummaryList, IList<PlayerSummaryDto>>
+{
+	private readonly ConnectionString _connectionString;
+
+	public GetPlayerSummaryListHandler(ConnectionString connectionString)
+	{
+		_connectionString = connectionString;
+	}
+
+	public IList<PlayerSummaryDto> Handle(GetPlayerSummaryList query)
+	{
+		var sql = GetSqlQuery();
+
+		using SqlConnection connection = new(_connectionString.Value);
+
+		return connection.Query<PlayerSummaryDto>(sql)
+			.ToList();
+	}
+
+	private static string GetSqlQuery()
+	{
+		return @"SELECT p.Id, p.FirstName + ' ' + p.LastName AS FullName,
+					p.Gender, p.Plays, p.Height, p.SinglesRank, p.DoublesRank
+				from Player p";
+	}
+}
