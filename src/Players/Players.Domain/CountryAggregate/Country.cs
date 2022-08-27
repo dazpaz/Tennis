d061f629ab96af10339268cd862ef5f2d186f1ae
Darren Powell <darren.powell@hotmail.co.uk>
2022-08-27 20:30:37 +0100

    Add Country Entity to the Domain

---
+++ src/Players/Players.Domain/CountryAggregate/Country.cs
@@ -0,0 +1,37 @@
+﻿using DomainDesign.Common;
+
+#nullable disable
+
+namespace Players.Domain.CountryAggregate;
+
+public class Country : AggregateRoot<CountryId>
+{
+	public string ShortName { get; private set; }
+
+	public string FullName { get; private set; }
+
+	protected Country()
+	{
+	}
+
+	private Country(CountryId id) : base(id)
+	{
+	}
+
+	public static Country Create(string shortName, string fullName)
+	{
+		var country = new Country(new CountryId())
+		{
+			ShortName = shortName,
+			FullName = fullName
+		};
+
+		return country;
+	}
+
+	public void Update(string shortName, string fullName)
+	{
+		ShortName = shortName;
+		FullName = fullName;
+	}
+}
---
+++ src/Players/Players.Domain/CountryAggregate/CountryId.cs
@@ -0,0 +1,10 @@
+﻿using DomainDesign.Common;
+
+namespace Players.Domain.CountryAggregate
+{
+	public sealed class CountryId : EntityId<CountryId>
+	{
+		public CountryId() : base() { }
+		public CountryId(Guid id) : base(id) { }
+	}
+}
