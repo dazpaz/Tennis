9ad2b1e02e7d9969022f46cbda99ad0c52642170
Darren Powell <darren.powell@hotmail.co.uk>
2022-08-29 18:06:48 +0100

    Correct issues with querying Players

--- src/Players/Players.Contract/PlayerSummaryDto.cs
+++ src/Players/Players.Contract/PlayerSummaryDto.cs
@@ -9,6 +9,7 @@ public class PlayerSummaryDto
 	public Gender Gender { get; set; }
 	public Plays Plays { get; set; }
 	public int Height { get; set; }
-	public int DoublesRank { get; set; }
 	public int SinglesRank { get; set; }
+	public int DoublesRank { get; set; }
+	public string CountryCode { get; set; } = string.Empty;
 }
