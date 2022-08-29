918f62662b84eff4d290e3646c592368df4d17e7
Darren Powell <darren.powell@hotmail.co.uk>
2022-08-29 20:12:21 +0100

    Introduce factories for creating commands and queries
    And use in the Players Controller

---
+++ src/Players/Players.WebApi/Factory/QueryFactory.cs
@@ -0,0 +1,17 @@
+﻿using CSharpFunctionalExtensions;
+using Players.Query;
+
+namespace Players.WebApi.Factory;
+
+public class QueryFactory : IQueryFactory
+{
+	public GetPlayerDetails CreateGetPlayerDetailsQuery(Guid id)
+	{
+		return new GetPlayerDetails(id);
+	}
+
+	public GetPlayerSummaryList CreateGetPlayerSummaryListQuery()
+	{
+		return new GetPlayerSummaryList();
+	}
+}
